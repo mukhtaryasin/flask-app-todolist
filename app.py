d8ec00514af2044809c31998a517e03579269638
Mukhtar Ahmed Yasin <mayasin252@gmail.com>
2025-04-25 23:05:36 +1000

    first commit

---
+++ app.py
@@ -0,0 +1,24 @@
+from flask import Flask, request, redirect, render_template
+
+app = Flask(__name__)
+todos = []
+
+@app.route('/')
+def index():
+    return render_template('index.html', todos=todos)
+
+@app.route('/add', methods=['POST'])
+def add():
+    task = request.form.get('task')
+    if task:
+        todos.append(task)
+    return redirect('/')
+
+@app.route('/delete/<int:index>')
+def delete(index):
+    if 0 <= index < len(todos):
+        todos.pop(index)
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
